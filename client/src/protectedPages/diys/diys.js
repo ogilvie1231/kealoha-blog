7aa14c8f6ddf2b4b7b249db348021a3c6c343cc8
ogilvie1231 <ogilvie1231@gmail.com>
2020-04-23 09:57:04 -0600

    setting up diy

--- client/src/protectedPages/diys/diys.js
+++ client/src/protectedPages/diys/diys.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
+import {
+  Card,
+  CardImg,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+} from "reactstrap";
 import CardCom from "../../components/Card/Card";
 import "./diys.css";
 import API from "../../utils/API";
-import images from '../../images/kealohaPH.jpg'
+import images from "../../images/kealohaPH.jpg";
 
-let DefaultImg = images
-console.log('DefaultImg ', DefaultImg)
+let DefaultImg = images;
+console.log("DefaultImg ", DefaultImg);
 
 class DiyP extends Component {
   state = {
@@ -27,6 +36,13 @@ class DiyP extends Component {
         console.log("diys loadAll error: ", error);
       });
   };
+
+  delete = (category, id) => {
+    API.deleteOne(id)
+      .then((res) => this.loadAll())
+      .catch((err) => console.log(err));
+  };
+
   render() {
     return (
       <div>
@@ -37,14 +53,27 @@ class DiyP extends Component {
         <div>
           {this.state.diys.length ? (
             this.state.diys.map((info) => (
-              <CardCom
-                className="videoCard"
-                key={info._id}
-                title={info.title}
-                subject={info.subject}
-                text={info.text}
-                src={info.file || DefaultImg}
-              />
+              <Card className="videoCard col-3" key={info._id}>
+                <CardImg top width="100%" src={DefaultImg} alt={info.title} />
+                <CardBody>
+                  <CardTitle>
+                    <h2>{info.title}</h2>
+                  </CardTitle>
+                  <CardSubtitle>
+                    <p>{info.subject}</p>
+                  </CardSubtitle>
+                  <CardText>
+                    <p>{info.text}</p>
+                  </CardText>
+                  <Button
+                    className="deleteButton"
+                    color="primary"
+                    onClick={() => this.delete(info.category, info._id)}
+                  >
+                    Delete
+                  </Button>
+                </CardBody>
+              </Card>
             ))
           ) : (
             <h3>No content at this time</h3>
